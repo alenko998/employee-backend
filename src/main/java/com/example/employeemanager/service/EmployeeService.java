bf52a890ee04d6acb6497164056d53b93a8b5188
alenko998 <alen_git@hotmail.com>
2023-08-30 13:01:29 +0200

    first

---
+++ src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.example.employeemanager.service;
+
+import com.example.employeemanager.exception.UserNotFoundException;
+import com.example.employeemanager.model.Employee;
+import com.example.employeemanager.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    @Autowired
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployes(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id).orElseThrow(()-> new UserNotFoundException("User by Id: " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteEmployeeById(id);
+    }
+}
